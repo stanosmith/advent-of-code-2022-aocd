b9c9294a1f886e803959189fb44a82e4bc0099ef
Stan Smith <stan@diamondfoundry.com>
2022-12-05 17:34:51 -0700

    Add Prettier config and reformat days

--- day_1.ts
+++ day_1.ts
@@ -1,24 +1,34 @@
-import { assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts";
-import { runPart } from "https://deno.land/x/aocd@v1.1.0/mod.ts";
-import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts"
+import { runPart } from "https://deno.land/x/aocd@v1.1.0/mod.ts"
+import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts"
 
 function parse(input: string) {
-  return input.trimEnd().split("\n\n").map((value) => value.split("\n").map(Number).reduce((a, b) => a+b)).flat().sort((a,b) => b-a);
+  return input
+    .trimEnd()
+    .split("\n\n")
+    .map((value) =>
+      value
+        .split("\n")
+        .map(Number)
+        .reduce((a, b) => a + b)
+    )
+    .flat()
+    .sort((a, b) => b - a)
 }
 
 function part1(input: string): number {
-  const items = parse(input);
+  const items = parse(input)
   return R.head(items)
 }
 
 function part2(input: string): number {
-  const items = parse(input);
+  const items = parse(input)
   return items.slice(0, 3).reduce((a, b) => a + b, 0)
 }
 
 if (import.meta.main) {
-  runPart(2022, 1, 1, part1);
-  runPart(2022, 1, 2, part2);
+  runPart(2022, 1, 1, part1)
+  runPart(2022, 1, 2, part2)
 }
 
 const TEST_INPUT = `\
@@ -36,12 +46,12 @@ const TEST_INPUT = `\
 9000
 
 10000
-`;
+`
 
 Deno.test("part1", () => {
-  assertEquals(part1(TEST_INPUT), 24000);
-});
+  assertEquals(part1(TEST_INPUT), 24000)
+})
 
 Deno.test("part2", () => {
-  assertEquals(part2(TEST_INPUT), 45000);
-});
+  assertEquals(part2(TEST_INPUT), 45000)
+})
--- day_2.ts
+++ day_2.ts
@@ -1,87 +1,87 @@
-import { assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts";
-import { runPart } from "https://deno.land/x/aocd@v1.1.0/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts"
+import { runPart } from "https://deno.land/x/aocd@v1.1.0/mod.ts"
 
 interface IShape {
-  shape: string;
-  points: number;
-  letters: string[];
+  shape: string
+  points: number
+  letters: string[]
 }
 
 const rock: IShape = {
   shape: "rock",
   points: 1,
   letters: ["A", "X"],
-};
+}
 const paper: IShape = {
   shape: "paper",
   points: 2,
   letters: ["B", "Y"],
-};
+}
 const scissors: IShape = {
   shape: "scissors",
   points: 3,
   letters: ["C", "Z"],
-};
+}
 
-const shouldLose = 'X'
-const shouldDraw = 'Y'
-const shouldWin = 'Z'
+const shouldLose = "X"
+const shouldDraw = "Y"
+const shouldWin = "Z"
 
 interface IPointMapping {
-  [key:string]: IShape
+  [key: string]: IShape
 }
-const rpsPointMapping:IPointMapping = {
+const rpsPointMapping: IPointMapping = {
   A: rock,
   [shouldLose]: rock,
   B: paper,
   [shouldDraw]: paper,
   C: scissors,
   [shouldWin]: scissors,
-};
+}
 
-const calcWin = calcTotalScore(6);
-const calcLose = calcTotalScore(0);
-const calcDraw = calcTotalScore(3);
+const calcWin = calcTotalScore(6)
+const calcLose = calcTotalScore(0)
+const calcDraw = calcTotalScore(3)
 
 function parse(input: string) {
   return input
     .trimEnd()
     .split("\n")
-    .map((game) => game.split(" ").map((gamePart) => gamePart.trimEnd()));
+    .map((game) => game.split(" ").map((gamePart) => gamePart.trimEnd()))
 }
 
 function part1(input: string): number {
-  const items = parse(input);
+  const items = parse(input)
   return items
     .map((game) => {
-      const player1 = rpsPointMapping[game[0]];
-      const player2 = rpsPointMapping[game[1]];
+      const player1 = rpsPointMapping[game[0]]
+      const player2 = rpsPointMapping[game[1]]
 
-      return getGamePoints(player1, player2);
+      return getGamePoints(player1, player2)
     })
-    .reduce((totalPoints, points) => totalPoints + points);
+    .reduce((totalPoints, points) => totalPoints + points)
 }
 
-function part2(input:string) {
-  const items = parse(input);
+function part2(input: string) {
+  const items = parse(input)
   return items
     .map((game) => {
-      const player1 = rpsPointMapping[game[0]];
-      const player2 = game[1];
+      const player1 = rpsPointMapping[game[0]]
+      const player2 = game[1]
 
-      return getGamePointsPartTwo(player1, player2);
+      return getGamePointsPartTwo(player1, player2)
     })
     .reduce((totalPoints, points) => totalPoints + points)
 }
 
 if (import.meta.main) {
-  runPart(2022, 2, 1, part1);
-  runPart(2022, 2, 2, part2);
+  runPart(2022, 2, 1, part1)
+  runPart(2022, 2, 2, part2)
 }
 
 function getGamePoints(player1: IShape, player2: IShape) {
   if (player1.shape === player2.shape) {
-    return calcDraw(player2.points);
+    return calcDraw(player2.points)
   }
 
   if (
@@ -92,15 +92,15 @@ function getGamePoints(player1: IShape, player2: IShape) {
     // 1 > 3: Rock defeats Scissors
     // 2 > 1: Paper defeats Rock
     // 3 > 2: Scissors defeats Paper
-    return calcWin(player2.points);
+    return calcWin(player2.points)
   } else {
-    return calcLose(player2.points);
+    return calcLose(player2.points)
   }
 }
 
-function getGamePointsPartTwo(player1:IShape, player2Strategy:string) {
+function getGamePointsPartTwo(player1: IShape, player2Strategy: string) {
   if (player2Strategy === shouldDraw) {
-    return calcDraw(player1.points);
+    return calcDraw(player1.points)
   }
 
   // 1 > 3: Rock defeats Scissors
@@ -111,53 +111,52 @@ function getGamePointsPartTwo(player1:IShape, player2Strategy:string) {
     switch (player1.shape) {
       case scissors.shape:
         winningShape = rock
-        break;
+        break
       case rock.shape:
         winningShape = paper
-        break;
+        break
       case paper.shape:
         winningShape = scissors
-        break;
+        break
       default:
         throw `Invalid shape "${player1.shape}"`
     }
-    return calcWin(winningShape.points);
+    return calcWin(winningShape.points)
   } else {
     let losingShape
     switch (player1.shape) {
       case scissors.shape:
         losingShape = paper
-        break;
+        break
       case rock.shape:
         losingShape = scissors
-        break;
+        break
       case paper.shape:
         losingShape = rock
-        break;
+        break
       default:
         throw `Invalid shape "${player1.shape}"`
     }
-    return calcLose(losingShape.points);
+    return calcLose(losingShape.points)
   }
 }
 
-
 function calcTotalScore(resultPoints: number) {
   return function winLoseDraw(rpsPoints: number) {
-    return rpsPoints + resultPoints;
-  };
+    return rpsPoints + resultPoints
+  }
 }
 
 const TEST_INPUT = `\
 A Y
 B X
 C Z
-`;
+`
 
 Deno.test("part1", () => {
-  assertEquals(part1(TEST_INPUT), 15);
-});
+  assertEquals(part1(TEST_INPUT), 15)
+})
 
 Deno.test("part2", () => {
-  assertEquals(part2(TEST_INPUT), 12);
-});
+  assertEquals(part2(TEST_INPUT), 12)
+})
