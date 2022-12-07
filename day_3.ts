28d0086bcd7e21a4ba791958d2a2bb0ffa691bff
Stan Smith <stan@diamondfoundry.com>
2022-12-06 18:05:14 -0700

    Solved: Day 3 - Part 2 🎄🎁

--- day_3.ts
+++ day_3.ts
@@ -9,31 +9,38 @@ function parse(input: string) {
   return input.trimEnd().split("\n")
 }
 
+const getPriorityLevel = (letter: string) => R.indexOf(letter, allItemTypes) + 1
+const splitCompartments = (compartment: string) => R.split("", compartment)
+const getCompartments = (rucksack: string) =>
+  R.splitAt(rucksack.length / 2, rucksack)
+const processGroups = (rucksack: string) => {
+  const compartmentsSplit = R.map(splitCompartments, getCompartments(rucksack))
+  return R.intersection(...compartmentsSplit)
+}
+const processGroups2 = (group: string[]) => {
+  const splitGroups = R.map(splitCompartments, group)
+  return R.reduce(R.intersection, splitGroups[0], splitGroups)
+}
+
 function part1(input: string): number {
   const items = parse(input)
-  const getPriorityLevel = (letter: string) =>
-    R.indexOf(letter, allItemTypes) + 1
-  const matchedItemTypes = R.flatten(
-    R.map((rucksack: string) => {
-      const rucksackCompartments = R.splitAt(rucksack.length / 2, rucksack)
-      const splitCompartments = (compartment: string) =>
-        R.split("", compartment)
-      const compartmentsSplit = R.map(splitCompartments, rucksackCompartments)
-      return R.intersection(...compartmentsSplit)
-    }, items),
-  )
+  const matchedItemTypes = R.flatten(R.map(processGroups, items))
   const priorityLevels = R.map(getPriorityLevel, matchedItemTypes)
   return R.reduce(R.add, 0, priorityLevels)
 }
 
 function part2(input: string): number {
   const items = parse(input)
-  throw new Error("TODO")
+  const groups = R.splitEvery(3, items)
+
+  const matchedItemTypes = R.flatten(R.map(processGroups2, groups))
+  const priorityLevels = R.map(getPriorityLevel, matchedItemTypes)
+  return R.reduce(R.add, 0, priorityLevels)
 }
 
 if (import.meta.main) {
   runPart(2022, 3, 1, part1)
-  // runPart(2022, 3, 2, part2);
+  runPart(2022, 3, 2, part2)
 }
 
 const TEST_INPUT = `\
@@ -49,6 +56,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 157)
 })
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 70)
+})
